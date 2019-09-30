ba95a8516bb7a998f0ef6f0876b186ed234edfe6
Miguel A Martinez <MichaelVMT@root.local>
2019-09-30 14:59:37 -0400

    n

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const Book = require('../models/book')
+
 
 //All books Home Page
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     res.render('books/index')
 })
 
