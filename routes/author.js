f198b3e475b0c58cdc750a1b34f12a7f954d0544
Juan Maquilon <juanmaquilon@Juans-MacBook-Pro.local>
2019-10-11 17:43:07 -0400

    Fixed author catch

--- routes/author.js
+++ routes/author.js
@@ -1,44 +1,46 @@
-const express = require('express')
-const router = express.Router()
-const Author = require('../models/author')
+const express = require('express');
+const router = express.Router();
+const Author = require('../models/author');
 
 //all authors
 router.get('/', async (req, res) => {
-    let searchOptions = {}
-    if (req.query.name != null && req.query.name != ''){
-        searchOptions.name = new RegExp(req.query.name, 'i')
-    }
-    try {
-        const authors = await Author.find({})
-        res.render('authors/index', { 
-            authors: authors, 
-            searchOptions: req.query
-        })
-    } catch {
-        res.redirect('/')
-    }
-})
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name != '') {
+    searchOptions.name = new RegExp(req.query.name, 'i');
+  }
+  try {
+    const authors = await Author.find({});
+    res.render('authors/index', {
+      authors: authors,
+      searchOptions: req.query
+    });
+  } catch (e) {
+    console.log('ERROR :', e);
+    res.redirect('/');
+  }
+});
 
 //new author authors
 router.get('/new', (req, res) => {
-    res.render('authors/new', { author: new Author() })
-})
+  res.render('authors/new', { author: new Author() });
+});
 
 //creating author
 router.post('/', async (req, res) => {
-    const author = new Author({
-        name: req.body.name
-    })
-    try {
-        const newAuthor = await author.save()
-        //res.redirect('authors/${newuthor.id}')
-        res.redirect('authors')
-    } catch {
-        res.render('authors/new', {
-            author : author, 
-            errorMessage: 'Error creating author'
-        })
-    }
-})
+  const author = new Author({
+    name: req.body.name
+  });
+  try {
+    const newAuthor = await author.save();
+    //res.redirect('authors/${newuthor.id}')
+    res.redirect('authors');
+  } catch (e) {
+    console.log('ERROR :', e);
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error creating author'
+    });
+  }
+});
 
-module.exports = router
+module.exports = router;
