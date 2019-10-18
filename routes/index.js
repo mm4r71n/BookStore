1b4357abceda5ca6ceb4b4ab6a907fecb620f34d
Juan Maquilon <juanmaquilon@Juans-MacBook-Pro.local>
2019-10-18 16:59:01 -0400

    Added fixed catch errors

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const router = express.Router()
-const Book = require('../models/book')
+const express = require('express');
+const router = express.Router();
+const Book = require('../models/book');
 
 router.get('/', async (req, res) => {
-    let books
-    try {
-        books = await Book.find({}).limit(10).exec()
+  let books;
+  try {
+    books = await Book.find({})
+      .limit(10)
+      .exec();
+  } catch (e) {
+    books = [];
+    console.log(e);
+  }
+  res.render('index', {
+    books: books
+  });
+});
 
-    } catch {
-        books = []
-    }
-    res.render('index', {
-        books: books
-    })
-})
-
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,43 +1,39 @@
-if (process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
 
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
-const indexRouter = require('./routes/index')
-const bookRouter = require('./routes/books')
-const cartRouter = require('./routes/cart')
-const userRouter = require('./routes/user')
-const wishlistRouter = require('./routes/wishlist')
-const authorRouter = require('./routes/author')
+const indexRouter = require('./routes/index');
+const bookRouter = require('./routes/books');
+const cartRouter = require('./routes/cart');
+const userRouter = require('./routes/user');
+const wishlistRouter = require('./routes/wishlist');
+const authorRouter = require('./routes/author');
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
-
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
-useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
+  useNewUrlParser: true
+});
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
 
+app.use('/', indexRouter);
+app.use('/books', bookRouter);
+app.use('/cart', cartRouter);
+app.use('/user', userRouter);
+app.use('/wishlist', wishlistRouter);
+app.use('/authors', authorRouter);
 
-app.use('/', indexRouter)
-app.use('/books', bookRouter)
-app.use('/cart', cartRouter)
-app.use('/user', userRouter)
-app.use('/wishlist', wishlistRouter)
-app.use('/authors', authorRouter)
-
-
-
-app.listen(process.env.PORT || 3000)
-
+app.listen(process.env.PORT || 3000);
