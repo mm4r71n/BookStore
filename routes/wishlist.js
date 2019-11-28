8ed3467147ef61ca9da3d90932f88a1533385678
Juan Maquilon <juanmaquilon@Juans-MacBook-Pro.local>
2019-11-27 20:11:23 -0500

    Wishlist edit

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -1,24 +1,24 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Wishlist = require("../models/wishlist");
-const Book = require("../models/book");
-const ObjectId = require("mongodb").ObjectId;
-const mongoose = require("mongoose");
+const Wishlist = require('../models/wishlist');
+const Book = require('../models/book');
+const ObjectId = require('mongodb').ObjectId;
+const mongoose = require('mongoose');
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const wishes = await Wishlist.find({
-      userId: ObjectId("5da9fbd86c2ebb1128a8eab0")
+      userId: ObjectId('5da9fbd86c2ebb1128a8eab0')
     })
-      .populate("user")
-      .exec((err, books) => { });
-    res.render("wishlist", { wishes: wishes });
+      .populate('user')
+      .exec((err, books) => {});
+    res.render('wishlist', { wishes: wishes });
   } catch (error) {
-    console.log("error on list", error);
+    console.log('error on list', error);
   }
 });
 
-router.get("/:bookId/:userId/:wishlistNum", async (req, res) => {
+router.get('/:bookId/:userId/:wishlistNum', async (req, res) => {
   // Save the bookId and the login to the wishlist collection
   const wishlist = new Wishlist({
     _id: new mongoose.Types.ObjectId(),
@@ -28,69 +28,81 @@ router.get("/:bookId/:userId/:wishlistNum", async (req, res) => {
   });
   try {
     const newWishlist = await wishlist.save();
-    res.redirect("/wishlist/list");
+    res.redirect('/wishlist/list');
   } catch (error) {
-    console.log("error on list", error);
+    console.log('error on list', error);
   }
 });
 
-router.get("/list", async (req, res) => {
+router.get('/list', async (req, res) => {
   const list = await Wishlist.aggregate([
     {
       $lookup: {
-        from: "books",
-        localField: "bookId",
-        foreignField: "_id",
-        as: "newWishList"
+        from: 'books',
+        localField: 'bookId',
+        foreignField: '_id',
+        as: 'newWishList'
       }
     }
-  ]).exec(function (err, wishes) {
+  ]).exec(async function(err, wishes) {
     if (err) {
-      console.log("ERROR", err);
+      console.log('ERROR', err);
     }
     var wishlist1 = [];
     var wishlist2 = [];
     var wishlist3 = [];
-    const newlist = wishes.map((wish) => {
+    const newlist = wishes.map(wish => {
       if (wish.wishlistNum === 1) {
-        wishlist1.push(wish)
+        wishlist1.push(wish);
       }
       if (wish.wishlistNum === 2) {
-        wishlist2.push(wish)
+        wishlist2.push(wish);
       }
       if (wish.wishlistNum === 3) {
-        wishlist3.push(wish)
+        wishlist3.push(wish);
       }
-    })
+    });
 
-    const newList = { wishlist1: wishlist1, wishlist2: wishlist2, wishlist3: wishlist3 }
+    const allwishlist = await Wishlist.find({});
 
-    res.render("wishlist/list", { wishes: newList });
+    console.log('allwishlist', allwishlist);
+
+    const newList = {
+      wishlist1: wishlist1,
+      wishlist2: wishlist2,
+      wishlist3: wishlist3
+    };
+
+    console.log('wishes', newList);
+
+    res.render('wishlist/list', { wishes: newList });
   });
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const wishlist = new Wishlist({
     userId: req.body.userId,
     bookId: req.body.bookId
   });
   try {
     const newWishlist = await wishlist.save();
-    res.redirect("/wishlist");
+    res.redirect('/wishlist');
   } catch (error) {
     renderNewPage(res, wishlist, true);
   }
 });
 
-router.get("/:id", async (req, res) => {
-  console.log("id", req.params.id.trim())
+router.get('/:id', async (req, res) => {
+  console.log('id', req.params.id.trim());
 
-  const res1 = await Wishlist.deleteOne({ _id: req.params.id.trim() }, function (err) {
+  const res1 = await Wishlist.deleteOne({ _id: req.params.id.trim() }, function(
+    err
+  ) {
     if (err) console.log(err);
-    console.log("Successful deletion");
-  })
+    console.log('Successful deletion');
+  });
 
-  res.redirect("/wishlist/list");
-})
+  res.redirect('/wishlist/list');
+});
 
 module.exports = router;
